950c121d085304599fea9e1f749415ae9078d60c
BigWing06 <166898297+BigWing06@users.noreply.github.com>
2025-02-04 08:24:52 -0500

    Inventory management improvements (#2)
    * Inventory Rework
    
    Cleaned up resourceInfo Dict
    ResourceInfo Dict now is based off of names instead of int values
    Created InventoryManagerScript to handle invenotory instead of having it spread around everywhere
    
    * Inventory Rework Cleanup
    
    Fixed issues after inventory rework

---
+++ playerResources/inventoryManager.gd
@@ -0,0 +1,46 @@
+extends Node
+
+var inventory = {}
+ #Is a list of all resrouces in the game
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var validResources
+
+# Called when the node enters the scene tree for the first time.
+func setup():
+	validResources = global.resourceInfo.keys()
+	for resource in global.resourceInfo.keys(): #Adds all of the valid resorucces to the dictionary as to not cause errors later
+		inventory[resource] = 0
+		
+func checkResource(resource):
+	if resource in validResources: #Checks to make sure resource is a valid resrouce otherwise throws error
+		return true
+	else:
+		push_error(str(resource) + " is not a valid resource string")
+		return false
+
+func add(resource, amount): #Adds an amount of a specific resrouce to inventory
+	if checkResource(resource):
+		inventory[resource] += amount
+
+func getAmount(resource): #Gets amount of a specific resource in inventory
+	if checkResource(resource):
+		return inventory[resource]
+		
+func hasAmount(amount, resource): #Checks to see if a specified amount of a resource is in the inventory
+	if checkResource(resource):
+		if amount <= getAmount(resource):
+			return true
+	return false
+		
+func getResourceDict(): #Returns a dictionary containg resouces and amounts that are in inventory
+	var output = {}
+	for resource in inventory.keys():
+		if inventory[resource] > 0:
+			output[resource] = inventory[resource]
+	return output
+		
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
